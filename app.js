502a204124d5a0241ab0536ae921b7ae71ec782c
Kevin Winata <kevinwinata@gmail.com>
2015-02-18 11:27:01 +0700

    Integrating material-ui

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
